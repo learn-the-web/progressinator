a4fef8c10bc8083eabb72ab3a75bb7ab6dc6d296
Thomas J Bradley <hey@thomasjbradley.ca>
2018-10-04 16:34:38 -0400

    Force a small set of choices for the section number

--- progressinator/core/models/user_profile.py
+++ progressinator/core/models/user_profile.py
@@ -1,14 +1,20 @@
 from django.conf import settings
 from django.db import models
+from progressinator.common.util import ChoiceEnum
 
+class UserProfileSectionChoices(ChoiceEnum):
+    SECTION_010 = '010'
+    SECTION_020 = '020'
+    SECTION_030 = '030'
+    SECTION_040 = '040'
 
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
     current_course = models.ForeignKey('Course', on_delete=models.CASCADE, blank=True, null=True)
-    current_section = models.CharField(max_length=3, blank=True, null=True)
+    current_section = models.CharField(choices=UserProfileSectionChoices.choices(), max_length=3, blank=True, null=True)
 
     def __str__(self):
-        return f'{self.user.username} — {self.assessment_uri} — {self.grade}'
+        return f'{self.user.username} — {self.current_course.slug} — {self.current_section}'
 
     class Meta:
         verbose_name = "user profile"
